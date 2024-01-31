154a6d70f72286ab4b855c6fd9928ac65951e6e6
Vadim <sagitov.vadim@gmail.com>
2024-01-31 20:54:56 +0300

    hw10: init (part 1)

--- src/main/scala/catsconcurrency/cats_effect_homework/Wallet.scala
+++ src/main/scala/catsconcurrency/cats_effect_homework/Wallet.scala
@@ -4,6 +4,16 @@ import cats.effect.Sync
 import cats.implicits._
 import Wallet._
 
+import java.io.File
+import java.nio.charset.StandardCharsets
+import java.nio.file._
+
+import scala.io._
+import scala.language.higherKinds
+
+
+
+
 // DSL управления электронным кошельком
 trait Wallet[F[_]] {
   // возвращает текущий баланс
@@ -25,9 +35,18 @@ trait Wallet[F[_]] {
 // - java.nio.file.Files.exists
 // - java.nio.file.Paths.get
 final class FileWallet[F[_]: Sync](id: WalletId) extends Wallet[F] {
-  def balance: F[BigDecimal] = ???
-  def topup(amount: BigDecimal): F[Unit] = ???
-  def withdraw(amount: BigDecimal): F[Either[WalletError, Unit]] = ???
+  override def balance: F[BigDecimal] = for {
+    balance <- readFile(id)
+  } yield balance
+  def topup(amount: BigDecimal): F[Unit] = for {
+    balanceValue <- this.balance
+    _ <- putDataToFile(id, balanceValue + amount)
+  } yield ()
+
+  def withdraw(amount: BigDecimal): F[Either[WalletError, Unit]] = for {
+    balanceValue <- this.balance
+    result <- if(balanceValue - amount > 0) putDataToFile(id, balanceValue - amount).map(Right(_)) else Sync[F].delay(Left(BalanceTooLow))
+  } yield result
 }
 
 object Wallet {
@@ -37,7 +56,41 @@ object Wallet {
   // Здесь нужно использовать обобщенную версию уже пройденного вами метода IO.delay,
   // вызывается она так: Sync[F].delay(...)
   // Тайпкласс Sync из cats-effect описывает возможность заворачивания сайд-эффектов
-  def fileWallet[F[_]: Sync](id: WalletId): F[Wallet[F]] = ???
+  def fileWallet[F[_]: Sync](id: WalletId): F[Wallet[F]] = for {
+    // создание нового кошелька
+    _ <- createFile(id)
+    // создание объекта кошелька
+    res <- Sync[F].delay(new FileWallet(id))
+  } yield res
+
+  private def createFile[F[_]: Sync](id: WalletId): F[Unit] = {
+    if(!Files.exists(Paths.get(id))) {
+      for {
+        // создание нового файла
+        _ <- Sync[F].delay(new File(id).createNewFile())
+        // инициализируем сумму в кошельке значением 0
+        _ <- putDataToFile(id, 0)
+      } yield ()
+    } else {
+      Sync[F].unit
+    }
+  }
+
+  def putDataToFile[F[_]: Sync](id: WalletId, value: BigDecimal): F[Unit] = Sync[F].bracket
+    { openFile(id) }
+    { _: BufferedSource =>
+      Sync[F].delay(Files.write(Paths.get(id), value.toString().getBytes(StandardCharsets.UTF_8))) *>
+        Sync[F].unit }
+    { x => closeFile(x) }
+
+  def readFile[F[_]: Sync](id: WalletId): F[BigDecimal] = Sync[F].bracket
+    { openFile(id) }
+    { v => Sync[F].delay(BigDecimal(v.getLines().mkString)) }
+    { x => closeFile(x) }
+
+  private def openFile[F[_]: Sync](id: WalletId): F[BufferedSource] = Sync[F].delay(Source.fromFile(id))
+
+  private def closeFile[F[_]: Sync](f: BufferedSource): F[Unit] = Sync[F].delay(f.close())
 
   type WalletId = String
 
--- src/main/scala/catsconcurrency/cats_effect_homework/WalletFibersApp.scala
+++ src/main/scala/catsconcurrency/cats_effect_homework/WalletFibersApp.scala
@@ -1,7 +1,12 @@
 package catsconcurrency.cats_effect_homework
 
-import cats.effect.{IO, IOApp}
+import cats.Monad
+import cats.effect._
 import cats.implicits._
+import Wallet._
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 // Поиграемся с кошельками на файлах и файберами.
 
@@ -21,10 +26,31 @@ object WalletFibersApp extends IOApp.Simple {
   def run: IO[Unit] =
     for {
       _ <- IO.println("Press any key to stop...")
-      wallet1 <- Wallet.fileWallet[IO]("1")
-      wallet2 <- Wallet.fileWallet[IO]("2")
-      wallet3 <- Wallet.fileWallet[IO]("3")
-      // todo: запустить все файберы и ждать ввода от пользователя чтобы завершить работу
+      wallet1 <- Wallet.fileWallet[IO]("1.txt")
+      wallet2 <- Wallet.fileWallet[IO]("2.txt")
+      wallet3 <- Wallet.fileWallet[IO]("3.txt")
+      f1 <- Spawn[IO].start(loop(topup(wallet1, 100)))
+      f2 <- Spawn[IO].start(loop(topup(wallet2, 500)))
+      f3 <- Spawn[IO].start(loop(topup(wallet3, 2000)))
+      printer <- Spawn[IO].start(loop(printBalances(wallet1, wallet2, wallet3)))
+      _ <- IO.readLine
+      _ <- f1.cancel *> f2.cancel *> f3.cancel *> printer.cancel
     } yield ()
 
+  private def topup(wallet: Wallet[IO], pause: Int): IO[Unit] = for {
+    _ <- wallet.topup(100)
+    res <- IO.sleep(pause milliseconds)
+  } yield res
+
+  private def printBalances(w1: Wallet[IO], w2: Wallet[IO], w3: Wallet[IO]): IO[Unit] = for {
+    _ <- w1.balance.flatMap(sum => IO.println(s"wallet1: $sum"))
+    _ <- w2.balance.flatMap(sum => IO.println(s"wallet2: $sum"))
+    _ <- w3.balance.flatMap(sum => IO.println(s"wallet3: $sum"))
+    _ <- IO.sleep(1 second)
+  } yield ()
+
+  private def loop(f: => IO[Unit]): IO[Unit] =
+    Monad[IO].whileM_(IO(true)) {
+      f
+    }
 }
