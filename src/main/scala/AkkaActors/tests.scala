f61639321a8dc55f5675bfa1c7a18a9956e4384b
ValentinShilin <valentin.shilin.de@gmail.com>
2024-02-10 20:24:23 +0100

    akka aktors

---
+++ src/main/scala/AkkaActors/tests.scala
@@ -0,0 +1,24 @@
+package AkkaActors
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class MySpec() extends  TestKit(ActorSystem("MySpec"))
+with ImplicitSender
+with AnyWordSpecLike
+with BeforeAndAfterAll
+{
+  "An echo actor" should {
+    "send block message" in {
+      val echo: ActorRef = system.actorOf(TestActors.echoActorProps)
+      echo ! "hello"
+      expectMsg("hello")
+    }
+  }
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+}
